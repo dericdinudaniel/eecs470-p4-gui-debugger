21b54e5b34d4241925891122c8365be9fdcfa9b9
Deric Dinu Daniel <dericdd@umich.edu>
2024-11-11 01:30:17 -0500

    more progress on BS Debugger

--- components/BranchStackDebugger.tsx
+++ components/BranchStackDebugger.tsx
@@ -15,20 +15,23 @@ const BranchStackDebugger: React.FC<BranchStackDebuggerProps> = ({
   const branch_stacks = extractSignalValue(signalBS, "branch_stacks").value;
   const BS_branch_stacks = parseCHECKPOINT_DATA_List(branch_stacks);
 
-  console.log(BS_branch_stacks);
-
   return (
     <>
-      <div className="p-4">test</div>
-      <div className="flex">
-        {BS_branch_stacks.map((checkpoint, idx) => {
-          console.log(checkpoint);
-          return (
-            <>
-              <div key={idx}></div>
-            </>
-          );
-        })}
+      <div className={`${className} mt-4`}>
+        <div className="bg-gray-500/[.15] rounded-lg shadow-lg p-4 inline-flex flex-col items-center">
+          <div className="p-4">test</div>
+          <div className="grid grid-cols-2 gap-x-2 gap-y-2">
+            {BS_branch_stacks.map((cp, idx) => {
+              return (
+                <div key={idx}>
+                  <div className="bg-gray-100 p-2 rounded-xl">
+                    {cp.rob_tail}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
     </>
   );
